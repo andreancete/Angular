0ff7436bb7c350eb48512e576b224c135dbf81ce
andreancete <andreancete@gmail.com>
2020-10-08 09:57:14 -0300

    andamento nos exemplos

---
+++ JavaScript/javascript-avancado/aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -0,0 +1,23 @@
+class NegociacaoController {
+    
+    constructor() {
+        
+        let $ = document.querySelector.bind(document);
+        this._inputData = $('#data');
+        this._inputQuantidade = $('#quantidade');
+        this._inputValor = $('#valor');
+    }
+    
+    adiciona(event) {
+        
+        event.preventDefault();
+
+        let helper = new DateHelper();
+    
+        let negociacao = new Negociacao(
+            helper.textoParaData(this._inputData.value),
+            this._inputQuantidade.value,
+            this._inputValor.value
+        );
+    }
+}
